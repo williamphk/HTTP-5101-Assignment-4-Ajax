b152d6c91195fe46358d35effe678ac148840606
William Poon <willphk@outlook.com>
2023-04-15 20:58:50 -0400

    Created Ajax for Update Teacher

--- js/delete.js
+++ js/delete.js
@@ -1,31 +1,30 @@
-const deleteTeacherForm = document.getElementById('deleteTeacherForm');
-const formData = new FormData(deleteTeacherForm);
-const data = Object.fromEntries(formData.entries());
+const deleteTeacherForm = document.getElementById("deleteTeacherForm");
 
-
-deleteTeacherForm.addEventListener('submit', function (event) {
-    event.preventDefault();
-    deleteTeacher();
+deleteTeacherForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const formData = new FormData(deleteTeacherForm);
+  const data = Object.fromEntries(formData.entries());
+  deleteTeacher(data);
 });
 
-function deleteTeacher() {
-    let URL = "http://localhost:59934/api/TeacherData/DeleteTeacher/" + data["TeacherId"];
-    let rq = new XMLHttpRequest();
+function deleteTeacher(data) {
+  let URL =
+    "http://localhost:59934/api/TeacherData/DeleteTeacher/" + data["TeacherId"];
+  let rq = new XMLHttpRequest();
 
-    rq.onreadystatechange = function () {
-        if (rq.readyState === 4) {
-            if (rq.status === 200 || rq.status === 204) {
-                console.log("Post successfully created!");
-            } else {
-                console.error("Error creating teacher:", rq.status, rq.statusText);
-            }
-        }
-    };
+  rq.onreadystatechange = function () {
+    if (rq.readyState === 4) {
+      if (rq.status === 200 || rq.status === 204) {
+        console.log("Post successfully created!");
+      } else {
+        console.error("Error creating teacher:", rq.status, rq.statusText);
+      }
+    }
+  };
 
-    rq.open("POST", URL);
-    rq.setRequestHeader('Content-type', 'text/plain');
+  rq.open("POST", URL);
+  rq.setRequestHeader("Content-type", "text/plain");
 
-    console.log("sending " + data["TeacherId"]);
-    rq.send(data["TeacherId"]);
+  console.log("sending " + data["TeacherId"]);
+  rq.send(data["TeacherId"]);
 }
-
